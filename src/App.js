b8944df20ecbc6947e23212a88c8e1c2f221b255
aditya <aditya@debian.debian>
2022-11-08 16:25:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as bodyPix from "@tensorflow-models/body-pix";
+import Webcam from 'react-webcam';
+
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runBodySegment = async () => {
+    const net = await bodyPix.load();
+    console.log("Bodypix model loaded");
+
+    setInterval(()=>{
+      detect(net);
+    },100);
+  };
+
+  const detect = async (net) => {
+    //check if data available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      //Get video properties
+      const video = webcamRef.current.video;
+      const videoHeight = video.videoHeight;
+      const videoWidth = video.videoWidth;
+
+      //set video width and height
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+      
+      //set canvas width and height
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      //Make detections
+      const person = await net.segmentPersonParts(video);
+      console.log(person);
+
+      //Draw detections
+      const coloredPartImage = bodyPix.toColoredPartMask(person);
+
+      bodyPix.drawMask(
+        canvasRef.current,
+        video,
+        coloredPartImage,
+        0.7,
+        0,
+        false
+      );
+    }
+
+
+  };
+
+  runBodySegment();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480
+          }}
+        />
+        <canvas ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480
+          }}
+        />
       </header>
     </div>
   );
